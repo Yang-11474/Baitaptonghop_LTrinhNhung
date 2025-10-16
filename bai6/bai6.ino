b7ec9e2622e3de58a479dcb149603441f4dc9884
Yang-11474 <236748833+Yang-11474@users.noreply.github.com>
2025-10-16 22:55:35 +0700

    Update bai6.ino

--- bai6/bai6.ino
+++ bai6/bai6.ino
@@ -6,12 +6,8 @@ int btnTat=6;
 int pre=LOW;
 int pre1=LOW;
 int pre2=LOW;
-<<<<<<< HEAD
 int column=0;
 bool trangthai=true;
-=======
-int column=6;
->>>>>>> fb9b852947ed04b0385df489a837fe5d6deb072e
 void setup() {
   // put your setup code here, to run once:
   pinMode(btnTrai,INPUT);
@@ -24,7 +20,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-<<<<<<< HEAD
   int current=digitalRead(btnTrai);
   int current1=digitalRead(btnPhai);
   int current2=digitalRead(btnTat);
@@ -54,66 +49,6 @@ void loop() {
     lcd.noDisplay();
   }
   pre2=current2;
-
-
-
-  // if(pre==LOW &&current1==HIGH)
-  // {
-  //   lcd.clear();
-  //   lcd.display();
-  //   column-=2;
-  //   lcd.setCursor(column,0);
-    
-  //   lcd.print("Hello");
-  //   delay(200);
-  // }
-  // pre=current1;
-  // if(pre1==LOW &&current2==HIGH)
-  // {
-  //   lcd.clear();
-  //   lcd.display();
-  //   column+=2;
-  //   lcd.setCursor(column,0);
-  //   lcd.print("Hello");
-  //   delay(200);
-  // }
-  // pre1=current2;
-  // if(pre2==LOW &&current3==HIGH)
-  // {
-  //   lcd.noDisplay();
-  // }
-  // pre2=current3;
-=======
-  int current1=digitalRead(btnTrai);
-  int current2=digitalRead(btnPhai);
-  int current3=digitalRead(btnTat);
-  if(pre==LOW &&current1==HIGH)
-  {
-    lcd.clear();
-    lcd.display();
-    column-=2;
-    lcd.setCursor(column,0);
-    
-    lcd.print("Hello");
-    delay(200);
-  }
-  pre=current1;
-  if(pre1==LOW &&current2==HIGH)
-  {
-    lcd.clear();
-    lcd.display();
-    column+=2;
-    lcd.setCursor(column,0);
-    lcd.print("Hello");
-    delay(200);
-  }
-  pre1=current2;
-  if(pre2==LOW &&current3==HIGH)
-  {
-    lcd.noDisplay();
-  }
-  pre2=current3;
->>>>>>> fb9b852947ed04b0385df489a837fe5d6deb072e
 
  
 }
